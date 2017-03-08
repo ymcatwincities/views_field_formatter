1f09c946349fe300d34214993d7c900b098a5292
Pol Dellaiera <Pol@47194.no-reply.drupal.org>
2017-03-08 23:01:50 +0100

    Do not display the field if the view is empty.

--- src/Plugin/Field/FieldFormatter/ViewsFieldFormatter.php
+++ src/Plugin/Field/FieldFormatter/ViewsFieldFormatter.php
@@ -210,13 +210,33 @@ class ViewsFieldFormatter extends FormatterBase {
     $elements = [];
     $settings = $this->getSettings();
     $cardinality = $items->getFieldDefinition()->getFieldStorageDefinition()->getCardinality();
-    list($view, $view_display) = explode('::', $settings['view'], 2);
+    list($view_id, $view_display) = explode('::', $settings['view'], 2);
+
+    $view = Views::getView($view_id);
+    if (!$view || !$view->access($view_display)) {
+      return $elements;
+    }
+
+    $view->setArguments($this->getArguments($items, $items[0], 0));
+    $view->setDisplay($view_display);
+    $view->preExecute();
+    $view->execute();
+
+    if (empty($view->result)) {
+      return $elements;
+    }
+
+    $elements = array(
+      '#cache' => array(
+        'max-age' => 0,
+      ),
+    );
 
     if (((bool) $settings['multiple'] === TRUE) && ($cardinality != 1)) {
       if (!empty($settings['implode_character'])) {
         $elements[0] = [
           '#type' => 'view',
-          '#name' => $view,
+          '#name' => $view_id,
           '#display_id' => $view_display,
           '#arguments' => $this->getArguments($items, NULL, 0),
         ];
@@ -225,7 +245,7 @@ class ViewsFieldFormatter extends FormatterBase {
         foreach ($items as $delta => $item) {
           $elements[$delta] = [
             '#type' => 'view',
-            '#name' => $view,
+            '#name' => $view_id,
             '#display_id' => $view_display,
             '#arguments' => $this->getArguments($items, $item, $delta),
           ];
@@ -235,7 +255,7 @@ class ViewsFieldFormatter extends FormatterBase {
     else {
       $elements[0] = [
         '#type' => 'view',
-        '#name' => $view,
+        '#name' => $view_id,
         '#display_id' => $view_display,
         '#arguments' => $this->getArguments($items, $items[0], 0),
       ];
